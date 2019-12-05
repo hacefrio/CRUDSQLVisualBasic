27b255edfd1ec84bf72578397e8d7016dfc4765a
hacefrio <vargas.sebastianm@gmail.com>
2019-12-05 13:18:55 -0300

    subida trabajo Signed-off-by: hacefrio <vargas.sebastianm@gmail.com>

---
+++ datosConSQLSERVEr/Form1.Designer.vb
@@ -0,0 +1,452 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.addsexo = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.addapema = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.addapepa = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.addnombre = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.addcodigo = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.sexo = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.apema = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.apepa = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.nombre = New System.Windows.Forms.TextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.txtcodigoeditar = New System.Windows.Forms.TextBox()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.TabPage5 = New System.Windows.Forms.TabPage()
+        Me.txteliminar = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TabPage6 = New System.Windows.Forms.TabPage()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.txtbuscar = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.DataGrid = New System.Windows.Forms.DataGridView()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        Me.TabPage5.SuspendLayout()
+        Me.TabPage6.SuspendLayout()
+        CType(Me.DataGrid, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Controls.Add(Me.TabPage5)
+        Me.TabControl1.Controls.Add(Me.TabPage6)
+        Me.TabControl1.Location = New System.Drawing.Point(0, 12)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(503, 367)
+        Me.TabControl1.TabIndex = 0
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.Button5)
+        Me.TabPage1.Controls.Add(Me.addsexo)
+        Me.TabPage1.Controls.Add(Me.Label7)
+        Me.TabPage1.Controls.Add(Me.addapema)
+        Me.TabPage1.Controls.Add(Me.Label6)
+        Me.TabPage1.Controls.Add(Me.addapepa)
+        Me.TabPage1.Controls.Add(Me.Label5)
+        Me.TabPage1.Controls.Add(Me.addnombre)
+        Me.TabPage1.Controls.Add(Me.Label4)
+        Me.TabPage1.Controls.Add(Me.addcodigo)
+        Me.TabPage1.Controls.Add(Me.Label3)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(495, 341)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Añadir"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(142, 190)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 10
+        Me.Button5.Text = "agregar"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'addsexo
+        '
+        Me.addsexo.Location = New System.Drawing.Point(117, 135)
+        Me.addsexo.Name = "addsexo"
+        Me.addsexo.Size = New System.Drawing.Size(100, 20)
+        Me.addsexo.TabIndex = 9
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(71, 138)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(29, 13)
+        Me.Label7.TabIndex = 8
+        Me.Label7.Text = "sexo"
+        '
+        'addapema
+        '
+        Me.addapema.Location = New System.Drawing.Point(117, 109)
+        Me.addapema.Name = "addapema"
+        Me.addapema.Size = New System.Drawing.Size(100, 20)
+        Me.addapema.TabIndex = 7
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(22, 112)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(85, 13)
+        Me.Label6.TabIndex = 6
+        Me.Label6.Text = "Apellido materno"
+        '
+        'addapepa
+        '
+        Me.addapepa.Location = New System.Drawing.Point(117, 83)
+        Me.addapepa.Name = "addapepa"
+        Me.addapepa.Size = New System.Drawing.Size(100, 20)
+        Me.addapepa.TabIndex = 5
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(22, 86)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(83, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Apellido paterno"
+        '
+        'addnombre
+        '
+        Me.addnombre.Location = New System.Drawing.Point(117, 57)
+        Me.addnombre.Name = "addnombre"
+        Me.addnombre.Size = New System.Drawing.Size(100, 20)
+        Me.addnombre.TabIndex = 3
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(61, 60)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(44, 13)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "Nombre"
+        '
+        'addcodigo
+        '
+        Me.addcodigo.Location = New System.Drawing.Point(117, 31)
+        Me.addcodigo.Name = "addcodigo"
+        Me.addcodigo.Size = New System.Drawing.Size(100, 20)
+        Me.addcodigo.TabIndex = 1
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(61, 34)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "codigo"
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.Button6)
+        Me.TabPage2.Controls.Add(Me.sexo)
+        Me.TabPage2.Controls.Add(Me.Label2)
+        Me.TabPage2.Controls.Add(Me.apema)
+        Me.TabPage2.Controls.Add(Me.Label8)
+        Me.TabPage2.Controls.Add(Me.apepa)
+        Me.TabPage2.Controls.Add(Me.Label9)
+        Me.TabPage2.Controls.Add(Me.nombre)
+        Me.TabPage2.Controls.Add(Me.Label10)
+        Me.TabPage2.Controls.Add(Me.txtcodigoeditar)
+        Me.TabPage2.Controls.Add(Me.Label11)
+        Me.TabPage2.Controls.Add(Me.Button4)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(495, 341)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Editar"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(177, 78)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(70, 21)
+        Me.Button6.TabIndex = 20
+        Me.Button6.Text = "comprobar"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'sexo
+        '
+        Me.sexo.Location = New System.Drawing.Point(160, 196)
+        Me.sexo.Name = "sexo"
+        Me.sexo.Size = New System.Drawing.Size(100, 20)
+        Me.sexo.TabIndex = 19
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(114, 199)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(29, 13)
+        Me.Label2.TabIndex = 18
+        Me.Label2.Text = "sexo"
+        '
+        'apema
+        '
+        Me.apema.Location = New System.Drawing.Point(160, 170)
+        Me.apema.Name = "apema"
+        Me.apema.Size = New System.Drawing.Size(100, 20)
+        Me.apema.TabIndex = 17
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(65, 173)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(85, 13)
+        Me.Label8.TabIndex = 16
+        Me.Label8.Text = "Apellido materno"
+        '
+        'apepa
+        '
+        Me.apepa.Location = New System.Drawing.Point(160, 144)
+        Me.apepa.Name = "apepa"
+        Me.apepa.Size = New System.Drawing.Size(100, 20)
+        Me.apepa.TabIndex = 15
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(65, 147)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(83, 13)
+        Me.Label9.TabIndex = 14
+        Me.Label9.Text = "Apellido paterno"
+        '
+        'nombre
+        '
+        Me.nombre.Location = New System.Drawing.Point(160, 118)
+        Me.nombre.Name = "nombre"
+        Me.nombre.Size = New System.Drawing.Size(100, 20)
+        Me.nombre.TabIndex = 13
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(104, 121)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(44, 13)
+        Me.Label10.TabIndex = 12
+        Me.Label10.Text = "Nombre"
+        '
+        'txtcodigoeditar
+        '
+        Me.txtcodigoeditar.Location = New System.Drawing.Point(160, 38)
+        Me.txtcodigoeditar.Name = "txtcodigoeditar"
+        Me.txtcodigoeditar.Size = New System.Drawing.Size(100, 20)
+        Me.txtcodigoeditar.TabIndex = 11
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(104, 41)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(39, 13)
+        Me.Label11.TabIndex = 10
+        Me.Label11.Text = "codigo"
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(179, 253)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(68, 27)
+        Me.Button4.TabIndex = 1
+        Me.Button4.Text = "Editar"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'TabPage5
+        '
+        Me.TabPage5.Controls.Add(Me.txteliminar)
+        Me.TabPage5.Controls.Add(Me.Label1)
+        Me.TabPage5.Controls.Add(Me.Button3)
+        Me.TabPage5.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage5.Name = "TabPage5"
+        Me.TabPage5.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage5.Size = New System.Drawing.Size(495, 341)
+        Me.TabPage5.TabIndex = 2
+        Me.TabPage5.Text = "Eliminar"
+        Me.TabPage5.UseVisualStyleBackColor = True
+        '
+        'txteliminar
+        '
+        Me.txteliminar.Location = New System.Drawing.Point(115, 57)
+        Me.txteliminar.Name = "txteliminar"
+        Me.txteliminar.Size = New System.Drawing.Size(100, 20)
+        Me.txteliminar.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(70, 60)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(40, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Codigo"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(115, 124)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 0
+        Me.Button3.Text = "eliminar"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TabPage6
+        '
+        Me.TabPage6.Controls.Add(Me.Button2)
+        Me.TabPage6.Controls.Add(Me.txtbuscar)
+        Me.TabPage6.Controls.Add(Me.Button1)
+        Me.TabPage6.Controls.Add(Me.DataGrid)
+        Me.TabPage6.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage6.Name = "TabPage6"
+        Me.TabPage6.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage6.Size = New System.Drawing.Size(495, 341)
+        Me.TabPage6.TabIndex = 3
+        Me.TabPage6.Text = "Ver"
+        Me.TabPage6.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(42, 309)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Actualizar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'txtbuscar
+        '
+        Me.txtbuscar.Location = New System.Drawing.Point(134, 312)
+        Me.txtbuscar.Name = "txtbuscar"
+        Me.txtbuscar.Size = New System.Drawing.Size(153, 20)
+        Me.txtbuscar.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(305, 312)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Buscar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'DataGrid
+        '
+        Me.DataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGrid.Location = New System.Drawing.Point(8, 3)
+        Me.DataGrid.Name = "DataGrid"
+        Me.DataGrid.Size = New System.Drawing.Size(484, 300)
+        Me.DataGrid.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(503, 384)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
+        Me.TabPage2.ResumeLayout(False)
+        Me.TabPage2.PerformLayout()
+        Me.TabPage5.ResumeLayout(False)
+        Me.TabPage5.PerformLayout()
+        Me.TabPage6.ResumeLayout(False)
+        Me.TabPage6.PerformLayout()
+        CType(Me.DataGrid, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents TabControl1 As System.Windows.Forms.TabControl
+    Friend WithEvents TabPage1 As System.Windows.Forms.TabPage
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents addsexo As System.Windows.Forms.TextBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents addapema As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents addapepa As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents addnombre As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents addcodigo As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TabPage2 As System.Windows.Forms.TabPage
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents TabPage5 As System.Windows.Forms.TabPage
+    Friend WithEvents txteliminar As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents TabPage6 As System.Windows.Forms.TabPage
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents txtbuscar As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents DataGrid As System.Windows.Forms.DataGridView
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents sexo As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents apema As System.Windows.Forms.TextBox
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents apepa As System.Windows.Forms.TextBox
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents nombre As System.Windows.Forms.TextBox
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents txtcodigoeditar As System.Windows.Forms.TextBox
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+
+End Class
---
+++ datosConSQLSERVEr/Form1.vb
@@ -0,0 +1,132 @@
+﻿Imports System.Data.SqlClient
+Imports System.Data.SqlClient.SqlException
+Imports System.Data.DataTableReader
+Imports System.Data.Odbc
+Public Class Form1
+    Dim conexion As String = "Server=localhost\SQLEXPRESS02;Database=master;Initial Catalog=persona;Trusted_Connection=True;"
+
+    Private Sub actualizarTabla()
+        Try
+            Dim dt As New DataTable
+
+            Dim cn As New SqlConnection
+            cn.ConnectionString = conexion
+            Using adaptador As New SqlDataAdapter("SELECT codigo, nombre, apellido_paterno, apellido_materno, sexo from persona", conexion)
+                adaptador.Fill(dt)
+            End Using
+            DataGrid.DataSource = dt
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        actualizarTabla()
+        MsgBox("Ejecutado correctamente")
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Try
+            Dim dt As New DataTable
+            Dim cn As New SqlConnection
+            cn.ConnectionString = conexion
+            Using adaptador As New SqlDataAdapter("SELECT codigo, nombre, apellido_paterno, apellido_materno, sexo from persona where codigo='" + txtbuscar.Text() + "'", conexion)
+                adaptador.Fill(dt)
+            End Using
+            DataGrid.DataSource = dt
+            MsgBox("Ejecutado correctamente")
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Try
+            Dim cn As New SqlConnection
+            cn.ConnectionString = conexion
+            Dim adaptador As New SqlCommand("delete from persona where codigo='" + txteliminar.Text() + "';", cn)
+            cn.Open()
+            adaptador.ExecuteNonQuery()
+            MsgBox("Ejecutado correctamente")
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button6.Click
+        Dim auxiliar As String
+        auxiliar = " "
+        Try
+            Dim cn As New SqlConnection
+            cn.ConnectionString = conexion
+            Dim adaptador1 As New SqlCommand("select nombre from persona where codigo = '" + txtcodigoeditar.Text() + "';", cn)
+            cn.Open()
+            auxiliar = adaptador1.ExecuteScalar()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+        nombre.Text = auxiliar
+        Try
+            Dim cn As New SqlConnection
+            cn.ConnectionString = conexion
+            Dim adaptador1 As New SqlCommand("select apellido_paterno from persona where codigo = '" + txtcodigoeditar.Text() + "';", cn)
+            cn.Open()
+            auxiliar = adaptador1.ExecuteScalar()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+        apepa.Text = auxiliar
+        Try
+            Dim cn As New SqlConnection
+            cn.ConnectionString = conexion
+            Dim adaptador1 As New SqlCommand("select apellido_materno from persona where codigo = '" + txtcodigoeditar.Text() + "';", cn)
+            cn.Open()
+            auxiliar = adaptador1.ExecuteScalar()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+        apema.Text = auxiliar
+        Try
+            Dim cn As New SqlConnection
+            cn.ConnectionString = conexion
+            Dim adaptador1 As New SqlCommand("select sexo from persona where codigo = '" + txtcodigoeditar.Text() + "';", cn)
+            cn.Open()
+            auxiliar = adaptador1.ExecuteScalar()
+            MsgBox("Ejecutado correctamente")
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+        sexo.Text = auxiliar
+    End Sub
+
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        Try
+            Dim cn As New SqlConnection
+            cn.ConnectionString = conexion
+            Dim adaptador As New SqlCommand("UPDATE persona SET nombre = @nombre ,apellido_paterno = @apepa ,apellido_materno = @apema, sexo= @sexo WHERE codigo =" + txtcodigoeditar.Text, cn)
+            adaptador.Parameters.Add("@nombre", SqlDbType.VarChar).Value = nombre.Text()
+            adaptador.Parameters.Add("@apepa", SqlDbType.VarChar).Value = apepa.Text()
+            adaptador.Parameters.Add("@apema", SqlDbType.VarChar).Value = apema.Text()
+            adaptador.Parameters.Add("@sexo", SqlDbType.VarChar).Value = sexo.Text()
+            cn.Open()
+            adaptador.ExecuteNonQuery()
+            MsgBox("Ejecutado correctamente")
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        Try
+            Dim cn As New SqlConnection
+            cn.ConnectionString = conexion
+            Dim adaptador As New SqlCommand("insert into persona values('" + addcodigo.Text() + "','" + addnombre.Text() + "','" + addapepa.Text() + "','" + addapema.Text() + "','" + addsexo.Text + "')", cn)
+            cn.Open()
+            adaptador.ExecuteNonQuery()
+            MsgBox("Ejecutado correctamente")
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+End Class
